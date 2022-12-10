c34701d3cba40826176a79642b35239be59fa56a
Kennytech01 <kennysonvictory@gmail.com>
2022-12-09 18:27:56 -0800

    task-12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,56 +17,47 @@ int main(void)
 
 {
 
-		int n1 = 48;
+	int n1 = 48;
 
-			int a = 0;
+	int a = 0;
 
-				int b;
+	int b;
 
-					int com = 44;
+	int com = 44;
 
+        while (a <= 99)
 
+{
 
-						while (a <= 99)
+	b = a + 1;
 
-								{
+	while (b <= 99)
 
-											b = a + 1;
+{
 
-													while (b <= 99)
+																		putchar((a / 10) + n1);
 
-																{
+																		putchar((a % 10) + n1);
 
-																				putchar((a / 10) + n1);
+																		putchar(32);
 
-																							putchar((a % 10) + n1);
+																		putchar((b / 10) + n1);     																	putchar((b % 10) + n1);
+ 																		if (a != 98 || b != 99)
 
-																										putchar(32);
+																	{
+																																			putchar(com);
+																																			putchar(32);
+																																		}
+																		b += 1;
 
-																													putchar((b / 10) + n1);
+																																		}
 
-																																putchar((b % 10) + n1);
+	a += 1;
 
-																																			if (a != 98 || b != 99)
+}
 
-																																							{
+	putchar('\n');
 
-																																												putchar(com);
-
-																																																putchar(32);
-
-																																																			}
-
-																																						b += 1;
-
-																																								}
-
-															a += 1;
-
-																}
-
-							putchar('\n');
-
-								return (0);
+	return (0);
 
 }
