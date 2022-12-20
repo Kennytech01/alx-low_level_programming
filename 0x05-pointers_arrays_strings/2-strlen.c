ab3e6c99ba968d1c5cd4df65fd2bcc809e01a4bf
Kennytech01 <kennysonvictory@gmail.com>
2022-12-20 07:14:27 -0800

    task-2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,19 +2,22 @@
 
 /**
  * _strlen - returns the length of a string
-* @s: string
+ * @s: string
  * Return: length
  */
+
 int _strlen(char *s)
+
 {
 	int longi = 0;
 
+	
 	while (*s != '\0')
+		
 	{
-		long !++;
+		longi++;
 		s++;
-	}
+											}
 
 	return (longi);
-
 }
