0c969c6e9c47d9ba390ed90f3aeb3f683e5c55e1
Kennytech01 <kennysonvictory@gmail.com>
2022-12-23 05:45:37 -0800

    task-8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 #include <stdio.h>
 
 
@@ -15,7 +14,7 @@ char *rot13(char *s)
 	int i;
 	int j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 		for (i = 0; s[i] != '\0'; i++)
 		{
 			for (j = 0; j < 52; j++)
