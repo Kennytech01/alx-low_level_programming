0c84a23959b541ed59a6f9d0bb38f124d9e2b0d2
Kennytech01 <kennysonvictory@gmail.com>
2022-12-23 02:43:08 -0800

    task -0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -26,11 +26,10 @@ char *_strcat(char *dest, char *src)
 	j = 0;
 	while (src[j] != '\0')
 	{
-												dest[i] = src[j];
-												i++;
-												j++;
-
-											}
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	dest[i] = '\0';
 
 	return (dest);
